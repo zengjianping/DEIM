c4cf9aa588185054941afd162ec4d73a63ae1469
zengjianping <happysjfzjp@126.com>
2025-07-11 11:00:10 +0800

    dev

--- scripts/infer_onnx.sh
+++ scripts/infer_onnx.sh
@@ -3,7 +3,7 @@
 INPUT=datas/images/image00/small-vehicles1.jpeg
 #INPUT=datas/videos/multi02.avi
 
-python tools/inference/onnx_inf.py \
-    --onnx datas/models/deim_dfine_hgnetv2_l_coco_50e.onnx \
+python tools/inference/onnx_inf_ndet.py \
+    --onnx datas/models/deim_dfine_hgnetv2_x_coco_50e.onnx \
     --input $INPUT
 
--- scripts/torch_to_onnx.sh
+++ scripts/torch_to_onnx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python tools/deployment/export_onnx_ndet.py --check \
-    -c configs/deim_dfine/deim_hgnetv2_l_coco.yml \
-    -r datas/models/deim_dfine_hgnetv2_l_coco_50e.pth
+python tools/deployment/export_onnx_ndet.py \
+    --check --simplify --max_dets 100 --thres_conf 0.3 \
+    -c configs/deim_dfine/deim_hgnetv2_x_coco.yml \
+    -r datas/models/deim_dfine_hgnetv2_x_coco_50e.pth
 
 
